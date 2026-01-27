38f2af358734a2f81d4dcf23808daddd36106a4b
Prashannt Bhandari <prashantbhandaricr07@gmail.com>
2026-01-27 15:47:57 +0530

    prepare frontend for deployement

--- url-shortner-frontend/src/pages/LandingPage.jsx
+++ url-shortner-frontend/src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import HeroSection from "../components/landing/HeroSection";
 import FeaturesSection from "../components/landing/FeaturesSection";
 import HowItWorks from "../components/landing/HowItWorks";
 import StatsSection from "../components/landing/StatsSection";
-import CTASection from "../components/landing/CTASection";
+import CTASection from "../components/landing/CtaSection";
 import Footer from "../components/Footer";
 
 const LandingPage = () => {
